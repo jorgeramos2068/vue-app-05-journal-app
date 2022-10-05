c971822ab1bc9eb1ce138ef457f57ab56522ef0a
Jorge Ramos <v-jorramos@expediagroup.com>
2022-10-05 14:00:38 -0500

    Create daybook module

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>Hello World</h1>
-    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-primary" @click="goToDaybook()">Primary</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToDaybook() {
+      this.$router.push({ name: 'daybook' });
+    },
+  },
+};
+</script>
