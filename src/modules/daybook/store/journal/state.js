2b2a74600c458e59393de325f79a81e360ace292
Jorge Ramos <v-jorramos@expediagroup.com>
2022-10-10 14:38:30 -0500

    Create sample entries for journal module

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,29 @@
 export default () => ({
   isLoading: false,
-  entries: [],
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 3000,
+      date: new Date().toDateString(),
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
+      picture: null,
+    },
+  ],
 });
