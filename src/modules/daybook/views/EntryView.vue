6e26c3f2fd6496fbb8db60d130fb87403dee25f2
Jorge Ramos <v-jorramos@expediagroup.com>
2022-10-13 10:52:28 -0500

    Add functionality to change an entry when the url is updated

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <span class="text-success fs-3 fw-bold">{{ month }}</span>
-      <span class="mx-1 fs-3">{{ day }}</span>
-      <span class="mx-2 fs-4 fw-light">{{ year }}</span>
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-success fs-3 fw-bold">{{ month }}</span>
+        <span class="mx-1 fs-3">{{ day }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ year }}</span>
+      </div>
+      <div>
+        <button class="btn btn-danger mx-2">
+          Delete <i class="fa fa-trash-alt"></i>
+        </button>
+        <button class="btn btn-primary">
+          Upload a photo <i class="fa fa-upload"></i>
+        </button>
+      </div>
     </div>
-    <div>
-      <button class="btn btn-danger mx-2">
-        Delete <i class="fa fa-trash-alt"></i>
-      </button>
-      <button class="btn btn-primary">
-        Upload a photo <i class="fa fa-upload"></i>
-      </button>
+    <hr />
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea
+        placeholder="What happened today?"
+        v-model="entry.text"
+      ></textarea>
     </div>
-  </div>
-  <hr />
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea
-      placeholder="What happened today?"
-      v-model="entry.text"
-    ></textarea>
-  </div>
+  </template>
   <Fab icon="fa-save" />
   <img
     src="https://th-thumbnailer.cdn-si-edu.com/Y99SIMiKe48gMhaOJVV71GaN0jw=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/a8/51/a851be96-2508-422f-88fa-5aee6fc7ada3/stand_in_cover_pic.jpg"
@@ -54,6 +56,7 @@ export default {
       const entry = this.getEntryById(this.id);
       if (!entry) {
         this.$router.push({ name: 'no-entry' });
+        return;
       }
       this.entry = entry;
     },
@@ -77,6 +80,11 @@ export default {
   },
   created() {
     this.loadEntry();
+  },
+  watch: {
+    id() {
+      this.loadEntry();
+    },
   },
 };
 </script>
