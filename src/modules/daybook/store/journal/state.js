02d8e7124763dda3a09b13f6d40c72b625194694
Jorge Ramos <v-jorramos@expediagroup.com>
2022-10-13 09:49:47 -0500

    Create getter to get an entry by id

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,25 +2,25 @@ export default () => ({
   isLoading: false,
   entries: [
     {
-      id: new Date().getTime(),
+      id: '01',
       date: new Date().toDateString(),
       text: 'Lorem1 ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
       picture: null,
     },
     {
-      id: new Date().getTime() + 1000,
+      id: '02',
       date: new Date().toDateString(),
       text: 'Lorem2 ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
       picture: null,
     },
     {
-      id: new Date().getTime() + 2000,
+      id: '03',
       date: new Date().toDateString(),
       text: 'Lorem3 ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
       picture: null,
     },
     {
-      id: new Date().getTime() + 3000,
+      id: '04',
       date: new Date().toDateString(),
       text: 'Lorem4 ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
       picture: null,
