a394d64b92df5138e36144a4da34b9d8cdf83683
Jorge Ramos <v-jorramos@expediagroup.com>
2022-10-17 18:48:36 -0500

    Add functionality to load entries through Vuex

--- src/modules/daybook/store/journal/mutations.js
+++ src/modules/daybook/store/journal/mutations.js
@@ -1,4 +1,7 @@
-export const setEntries = (/*state*/) => {};
+export const setEntries = (state, entries) => {
+  state.entries = [...state.entries, ...entries];
+  state.isLoading = false;
+};
 
 export const updateEntry = (/*state*/) => {};
 
