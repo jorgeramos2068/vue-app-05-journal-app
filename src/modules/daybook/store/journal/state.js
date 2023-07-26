b11bc988706ce4312472c6d288134dc900ec4147
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 19:16:15 -0600

    Add functionality to load entries through Vuex

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,29 +1,4 @@
 export default () => ({
-  isLoading: false,
-  entries: [
-    {
-      id: '01',
-      date: new Date().toDateString(),
-      text: 'Lorem1 ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
-      picture: null,
-    },
-    {
-      id: '02',
-      date: new Date().toDateString(),
-      text: 'Lorem2 ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
-      picture: null,
-    },
-    {
-      id: '03',
-      date: new Date().toDateString(),
-      text: 'Lorem3 ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
-      picture: null,
-    },
-    {
-      id: '04',
-      date: new Date().toDateString(),
-      text: 'Lorem4 ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu malesuada ante. Quisque magna sapien, tincidunt vel ornare sed, pharetra sit amet sapien. Nam pellentesque augue sit amet purus viverra fermentum.',
-      picture: null,
-    },
-  ],
+  isLoading: true,
+  entries: [],
 });
