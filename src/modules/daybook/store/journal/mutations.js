f93d76639b9ea86ec05c9fb4bb0f97ec67e42852
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 19:16:16 -0600

    Add functionality to update an entry

--- src/modules/daybook/store/journal/mutations.js
+++ src/modules/daybook/store/journal/mutations.js
@@ -3,6 +3,11 @@ export const setEntries = (state, entries) => {
   state.isLoading = false;
 };
 
-export const updateEntry = (/*state*/) => {};
+export const updateEntry = (state, updatedEntry) => {
+  const index = state.entries.find(entry => entry.id === updateEntry.id);
+  if (index >= 0) {
+    state.entries[index] = updatedEntry;
+  }
+};
 
 export const addEntry = (/*state*/) => {};
