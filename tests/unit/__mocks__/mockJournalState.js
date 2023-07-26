88cf22a995857e6ec500a6326dcae69c396fd93c
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 19:16:21 -0600

    Add tests for Vuex initial state

---
+++ tests/unit/__mocks__/mockJournalState.js
@@ -0,0 +1,20 @@
+export const mockJournalState = {
+  isLoading: false,
+  entries: [
+    {
+      id: 'test-id-01',
+      date: 1627077227978,
+      text: 'Test text 01',
+    },
+    {
+      id: 'test-id-02',
+      date: 1627077227979,
+      text: 'Test text 02',
+    },
+    {
+      id: 'test-id-03',
+      date: 1627077227980,
+      text: 'Test text 03',
+    },
+  ],
+};
